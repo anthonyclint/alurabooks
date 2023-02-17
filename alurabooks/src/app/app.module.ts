b5baff84d5553af6c9a2963f8e6e677da84ed29b
Anthony Clint Pastorello <dev.anthonyclint@gmail.com>
2023-02-17 11:17:00 -0300

    validação do campo de email.

--- alurabooks/src/app/app.module.ts
+++ alurabooks/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent, 
+    HeaderComponent, 
+    FooterComponent, 
+    CadastroComponent, 
+    SucessoCadastroComponent
+  ],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
